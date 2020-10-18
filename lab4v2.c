9bc12db5d9ae9c8283b2a6cf5c13e047ab69f311
Jacob Higgins <higginsjake909@gmail.com>
2020-10-18 17:52:06 -0500

    sharing to linux

--- lab4v2.c
+++ lab4v2.c
@@ -8,8 +8,12 @@
 #include <sched.h>
 #include <time.h>
 
+// Defines
 #define BILLION 1000000000L
 #define MILLISECONDS 1000000
+
+// Variables
+u_int64_t before_data_and_time[2];
 
 // read buttonPress pipe
 void * readBP_Pipe(void * arg )
@@ -38,6 +42,7 @@ int main(void)
 
     //testing
     int interrupt = 0;
+	int gps_data = 0;
 
     // mkfifo("/tmp/BP_pipe", 777); // returns 0 if successful?
 
@@ -48,35 +53,54 @@ int main(void)
 
     // create thread to reap BP pipe
 
-
     while(1)
     {
 
-            /* read from /tmp/N_pipe1 */
+        /* read from /tmp/N_pipe1 */
         // read(gpsd, &gpsBuffer, 1);
 
         /* Create timestamp for each read */
 		clock_gettime(CLOCK_MONOTONIC_RAW, &before);
-        printf("Before Stamp: %lu \n", before.tv_nsec);
-
-
-        if(interrupt == 2)
-        {
-		    clock_gettime(CLOCK_MONOTONIC_RAW, &after);
-            printf("After Stamp:  %lu \n", after.tv_nsec);
-
-		    timeDiff = (after.tv_sec - before.tv_sec) * 1000000 + (after.tv_nsec - before.tv_nsec);	
-    		printf("Difference between timestamps: %llu ns \n",(long long unsigned int) timeDiff);
-
-            interrupt = 0;
-        }
-
+		before_data_and_time[0] = before.tv_nsec;
+		before_data_and_time[1] = gps_data;
+        printf("Before: Stamp %lu | Data %lu \n", before_data_and_time[0], before_data_and_time[1]);
 
         // wait 250 ms?
         usleep(250000);
-        interrupt++;
+        gps_data++;
     }
 
 
     return 0;
+
 }
+
+	/* OLD CODE FOR LATER USE */
+
+    // while(1)
+    // {
+
+    //         /* read from /tmp/N_pipe1 */
+    //     // read(gpsd, &gpsBuffer, 1);
+
+    //     /* Create timestamp for each read */
+	// 	clock_gettime(CLOCK_MONOTONIC_RAW, &before);
+    //     printf("Before Stamp: %lu \n", before.tv_nsec);
+
+
+    //     if(interrupt == 2)
+    //     {
+	// 	    clock_gettime(CLOCK_MONOTONIC_RAW, &after);
+    //         printf("After Stamp:  %lu \n", after.tv_nsec);
+
+	// 	    timeDiff = (after.tv_sec - before.tv_sec) * 1000000 + (after.tv_nsec - before.tv_nsec);	
+    // 		printf("Difference between timestamps: %llu ns \n",(long long unsigned int) timeDiff);
+
+    //         interrupt = 0;
+    //     }
+
+
+    //     // wait 250 ms?
+    //     usleep(250000);
+    //     interrupt++;
+    // }
